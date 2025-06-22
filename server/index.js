0c52c809760dea765fe3c424ef5308e45b05f44a
wh0isjack <allancgadelha@gmail.com>
2025-06-22 15:16:46 -0300

    feat: add formParser | minor updates

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
 import uploadRoutes from "./routes/upload.js";
+import formParser from "./routes/formParser.js";
+
 
 const app = express();
 app.use(cors());
 app.use("/", uploadRoutes);
+app.use("/", formParser);
 
 app.listen(3001, () => console.log("Backend running at 3001 port"));
